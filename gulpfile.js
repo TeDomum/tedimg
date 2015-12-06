6f848150b50df2a542af36fbfa4e4f6cc6410d8e
Pierre Jaury <pierre@jaury.eu>
2015-12-06 20:46:42 +0100

    Simplify the main form and the architecture

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'), 
-    sass = require('gulp-ruby-sass') 
-    notify = require("gulp-notify") 
-    bower = require('gulp-bower');
+    bower = require('gulp-bower'),
+    replace = require('gulp-replace'),
+    concat = require('gulp-concat');
 
 var config = {
      srcPath: './resources',
@@ -9,20 +9,34 @@ var config = {
     dstPath: './tedimg/static'
 }
 
-var cssPath = [
-    config.srcPath,
-    config.bowerDir + '/Materialize/sass',
-]
-
 gulp.task('bower', function() { 
     return bower()
      .pipe(gulp.dest(config.bowerDir)) 
 });
 
 gulp.task('styles', function () {
-    return sass(config.srcPath + '/main.scss', {loadPath: cssPath})
-    .on('error', sass.logError)
-    .pipe(gulp.dest(config.dstPath))
+    return gulp.src([
+        config.bowerDir + '/*/dist/css/*.min.css',
+        config.srcPath + '/main.css'
+      ])
+      .pipe(concat('main.css'))
+      .pipe(replace("/font/", "/static/font/"))
+      .pipe(gulp.dest(config.dstPath))
 });
 
-  gulp.task('default', ['bower', 'styles']);
+gulp.task('scripts', function() {
+    return gulp.src([
+        config.bowerDir + '/jquery/dist/jquery.min.js',
+        config.bowerDir + '/Materialize/dist/js/materialize.min.js',
+        config.srcPath + '/*.js'
+      ])
+      .pipe(concat('main.js'))
+      .pipe(gulp.dest(config.dstPath))
+});
+
+gulp.task('copy', function() {
+    return gulp.src(config.bowerDir + '/Materialize/dist/font/**/*')
+    .pipe(gulp.dest(config.dstPath + '/font'));
+});
+
+  gulp.task('default', ['bower', 'styles', 'copy', 'scripts']);
